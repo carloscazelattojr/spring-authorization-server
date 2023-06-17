db8905a1156a850a2ff4653284d85dbf6a9e8bbb
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-06-17 17:51:08 -0300

    Project Authorization-server

---
+++ auth-server/src/main/java/br/com/carlosjunior/authserver/UserStoreConfig.java
@@ -0,0 +1,24 @@
+package br.com.carlosjunior.authserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserStoreConfig {
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        userDetailsManager.createUser(
+                User.withUsername("user")
+                        .password("{noop}password")
+                        .roles("USER")
+                        .build()
+        );
+        return userDetailsManager;
+    }
+
+}
