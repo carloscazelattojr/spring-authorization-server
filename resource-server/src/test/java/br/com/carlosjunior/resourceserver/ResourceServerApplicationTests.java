d0e62e200ce89108773b3b50088e563bfbd785f2
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2023-06-18 16:14:40 -0300

    finished project

---
+++ resource-server/src/test/java/br/com/carlosjunior/resourceserver/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carlosjunior.resourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
